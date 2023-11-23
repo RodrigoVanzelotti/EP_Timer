af3f573898ed47dfc6b70a11c4480919ef211aff
RodrigoVanzelotti <rodrigovanzelotti@gmail.com>
2023-11-23 16:07:10 -0300

    first commit

---
+++ DeployWithRender/src/app.py
@@ -0,0 +1,42 @@
+from dash import dcc, Output, Input, Dash, html
+from datetime import datetime, timedelta
+import dash_bootstrap_components as dbc
+
+app = Dash(__name__)
+server = app.server
+
+
+# Defina a data e hora do término do countdown
+target_date = datetime(2024, 5, 30, 23, 59, 59)
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = dbc.Container([
+        dbc.Row([
+            dbc.Col(html.H1("Faltam 👇 pro EP", className="text-center mt-5"))
+        ]),
+        dbc.Row([
+            dbc.Col(html.H2(id="timer", style={"fontSize": "96px", "fontWeight": "bold"}, className="text-center mt-5"))
+        ]),
+        dbc.Container([
+            html.A('@AAEE    -', href='https://www.instagram.com/aaee.ufrgs/'),
+            html.A('    @TOM    -', href='https://www.instagram.com/tom.ufrgs/'),
+            html.A('    @Vanze', href='https://www.instagram.com/r7vanzelotti/'),
+        ], className='footer'),
+        dcc.Interval(id='interval-component', interval=1000, n_intervals=0),
+], fluid=True, style={"display": "flex", "flexDirection": "column", "justifyContent": "center", "height": "100vh"})
+
+@app.callback(
+    Output('timer', 'children'),
+    Input('interval-component', 'n_intervals')
+)
+def update_timer(n):
+    # Calcula a diferença de tempo
+    time_left = target_date - datetime.now()
+    if time_left.total_seconds() > 0:
+        return str(time_left).split('.')[0]  # Remove a parte dos microssegundos
+    else:
+        return "Countdown Finished!"
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
