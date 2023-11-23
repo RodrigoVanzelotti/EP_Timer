b724f350a4b20f4c72d2bd25762c48139cde9604
RodrigoVanzelotti <rodrigovanzelotti@gmail.com>
2023-11-23 17:09:47 -0300

    fixed it all

--- src/app.py
+++ src/app.py
@@ -19,12 +19,18 @@ app.layout = dbc.Container([
         dbc.Row([
             dbc.Col(html.H2(id="timer", style={"fontSize": "96px", "fontWeight": "bold"}, className="text-center mt-5"))
         ]),
-        dbc.Container([
-            html.A('@AAEE    -', href='https://www.instagram.com/aaee.ufrgs/'),
-            html.A('    @TOM    -', href='https://www.instagram.com/tom.ufrgs/'),
-            html.A('    @Vanze', href='https://www.instagram.com/r7vanzelotti/'),
-        ], className='footer'),
-        dcc.Interval(id='interval-component', interval=1000, n_intervals=0),
+        dbc.Row([
+            dbc.Col([
+                html.A('AAEE', href='https://www.instagram.com/aaee.ufrgs/'),
+            ], sm=1),
+            dbc.Col([
+                html.A('TOM', href='https://www.instagram.com/tom.ufrgs/'),
+            ], sm=1),
+            dbc.Col([
+                html.A('Vanze', href='https://www.instagram.com/r7vanzelotti/'),
+            ], sm=1)
+        ], className='footer', justify='center'),
+        dcc.Interval(id='interval-component', interval=10000),
 ], fluid=True, style={"display": "flex", "flexDirection": "column", "justifyContent": "center", "height": "100vh"})
 
 @app.callback(
@@ -35,7 +41,13 @@ def update_timer(n):
     # Calcula a diferença de tempo
     time_left = target_date - datetime.now()
     if time_left.total_seconds() > 0:
-        return str(time_left).split('.')[0]  # Remove a parte dos microssegundos
+        # print(time_left)
+        try:
+            pattern = str(time_left).split('.')[0].split(':')[0:2]
+            return 'h'.join(pattern).replace("days", "dias") 
+        except:
+            pattern = str(time_left).split('.')[0].split(':')[0:2]
+            return 'h'.join(pattern).replace("day", "dia") 
     else:
         return "EP PORRA!"
 
